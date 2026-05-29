8f89e9619c7941624dcd40f80a22e07b4f436eaf
Sávio Monteiro <saviomonteiro2005@gmail.com>
2026-05-28 19:52:25 -0500

    exercicio while

---
+++ exercícios/repete.java
@@ -0,0 +1,12 @@
+package exercícios;
+
+public class repete {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 3) {
+            System.out.println("Olá");
+            contador++;
+        }
+    }
+
+}
